f7ed5d845d03c30d3534c0c530a3c5654e671d61
Vitor Cesario <vitorsousa676@gmail.com>
2023-09-11 14:56:08 -0300

    Menu

---
+++ Projeto_final_bloco_1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Projeto_final_bloco_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opção = 1;
+
+            while (opção != 6)
+            {
+
+                Console.WriteLine("*****************************************************");
+                Console.WriteLine("                    Loja De Jogos do Vitor                                 ");
+                Console.WriteLine("*****************************************************");
+
+                Console.WriteLine(" 1 - Criar Jogo");
+                Console.WriteLine(" 2 - Listar Todos os Jogos");
+                Console.WriteLine(" 3 - Buscar jogo Pelo Número");
+                Console.WriteLine(" 4 - Atualizar jogos ");
+                Console.WriteLine(" 5 - Apagar jogos");
+                Console.WriteLine(" 6 - Sair");
+
+                Console.WriteLine("*****************************************************");
+                Console.WriteLine("                  Digite a Sua Opção                 ");
+                Console.WriteLine("                                                     ");
+                Console.WriteLine("*****************************************************");
+                opção = Convert.ToInt32(Console.ReadLine());
+
+                if (opção == 6)
+                {
+                    Console.WriteLine("\nValeu e até mais");
+                    System.Environment.Exit(0);
+                }
+            }
+            switch (opção)
+            {
+                case 1:
+                    Console.WriteLine("Crie o seu jogo");
+                    break;
+                case 2:
+                    Console.WriteLine("Liste os seus jogos");
+                    break;
+                case 3:
+                    Console.WriteLine("Buscar jogos pelo numero");
+                    break;
+                case 4:
+                    Console.WriteLine("Atulizar lista de jogos");
+                    break;
+                case 5:
+                    Console.WriteLine("Apagar os Jogos");
+                    break;
+            }
+        }
+    }
+}
--- Projeto_final_bloco_1/obj/Debug/net7.0/Projeto_final_bloco_1.AssemblyInfo.cs
+++ Projeto_final_bloco_1/obj/Debug/net7.0/Projeto_final_bloco_1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Loja de Games")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_final_bloco_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Loja de Games")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Loja de Games")]
+[assembly: System.Reflection.AssemblyProductAttribute("Projeto_final_bloco_1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Projeto_final_bloco_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
