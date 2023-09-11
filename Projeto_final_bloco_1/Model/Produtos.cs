ad96edc03482da28c9517d11aa182e56645d12a8
Vitor Cesario <vitorsousa676@gmail.com>
2023-09-11 15:57:09 -0300

    Repository e Model

---
+++ Projeto_final_bloco_1/Model/Produtos.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_final_bloco_1.Model
+{
+    public class Produtos
+    {
+        private string nome;
+        private decimal preco;
+        private string notas;
+        private string descricao;
+        private string marca;
+
+        public Produtos(string nome, decimal preco, string notas, string descricao, string marca)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.notas = notas;
+            this.descricao = descricao;
+            this.marca = marca;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+
+        public decimal Getpreco()
+        {
+            return preco;
+        }
+
+        public void Setpreco(decimal preco)
+        {
+            this.preco = preco;
+        }
+
+        public string Getnotas()
+        {
+            return notas;
+        }
+
+        public void Setnota(string notas)
+        {
+            this.notas = notas;
+        }
+        public string GetDescricao()
+        {
+            return descricao;
+        }
+
+        public void SetDescricao(string descricao)
+        {
+            this.descricao = descricao;
+        }
+
+        public string GetMarca()
+        {
+            return marca;
+        }
+
+        public void SetMarca(string marca)
+        {
+            this.marca = marca;
+        }
+
+
+        public virtual void Visualizar()
+        {
+
+
+            Console.WriteLine("****************************");
+            Console.WriteLine("Loja do Vitor");
+            Console.WriteLine("****************************");
+            Console.WriteLine($"Nome do Produto {this.nome}");
+            Console.WriteLine($"Preço do Produto {this.preco}");
+            Console.WriteLine($"Notas do Produto {this.notas}");
+            Console.WriteLine($"Descrição do Produto {this.descricao}");
+            Console.WriteLine($"Marca do Produto {this.marca}");
+        }
+    }
+}
---
+++ Projeto_final_bloco_1/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_final_bloco_1.Repository
+{
+    internal interface IRepository
+    {
+    }
+}
